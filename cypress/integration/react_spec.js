cdc0741dca5316eb4920ccdf6be7a86896ee6f16
duvanmonsa <duvanmonsa@gmail.com>
2018-02-28 23:31:17 -0500

    Added api config

--- cypress/integration/react_spec.js
+++ cypress/integration/react_spec.js
@@ -1,11 +1,18 @@
 describe('Testing React App', function() {
     it('Open local site ', function() {
         cy.visit('http://localhost:3000/')
-        cy.get('.App-intro').contains('Testing Code')
+        cy.get('.user-name').contains('none')
     })
 
     it('Test a item is visible', function() {
         cy.visit('http://localhost:3000/')
         cy.get('.App-intro').should('be.visible')
     })
+
+    it('Test Button', function() {
+        cy.visit('http://localhost:3000/')
+        cy.get('.get-button').click()
+        cy.get('.user-name').contains('duvan monsa')
+    })
+
 })
--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        user: 'none'
+    };
+    this.onClick = this.onClick.bind(this);
+
+  }
+  onClick(){
+
+      const newthis = this;
+      axios.get('http://localhost:4000/users')
+          .then(function (response) {
+              newthis.setState({user: response.data.user})
+          })
+          .catch(function (error) {
+              console.log(error);
+          });
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +31,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-            Testing Code
+            <input type="button" value="Get User Name" onClick={this.onClick} className="get-button"/>
+            <div className="user-name">
+                {this.state.user}
+            </div>
+
         </p>
       </div>
     );
